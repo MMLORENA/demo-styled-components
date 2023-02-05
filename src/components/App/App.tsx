263a62810c99b392b46d4ea93a458fb83c89884d
Lorena Martinez Moledo <lmartmol@gmail.com>
2023-02-05 21:05:34 +0100

    Add files demo styled-components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,21 @@
+import Button from "../Button/Button";
+import Card from "../Card/Card";
 import "./App.css";
 
 const App = (): JSX.Element => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Button isDisabled={false} text="Life" type="button" size="small" />
+      <Button
+        isDisabled={false}
+        text="Life or Death"
+        type="button"
+        size="big"
+      />
+      <Button isDisabled={true} text="Death" type="button" size="big" />
+      <Card />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,19 @@
+import { screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import Button from "./Button";
+
+describe("Given a Button", () => {
+  describe("When it receives a text 'hello'", () => {
+    test("Then it shoul show a button with a text inside 'hello'", () => {
+      const buttonText = "hello";
+
+      render(
+        <Button isDisabled={false} size="big" type="button" text={buttonText} />
+      );
+
+      const button = screen.getByRole("button", { name: buttonText });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
